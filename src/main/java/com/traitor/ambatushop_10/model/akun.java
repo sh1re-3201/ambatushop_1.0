7e1952a070e842a639439ad6e2b8d9cc5f4f273f
sh1re-3201 <anugrahmapia@gmail.com>
2025-10-01 10:45:23 +0700

    Feat: Added model package added model class akun

---
+++ src/main/java/com/traitor/ambatushop_10/model/akun.java
@@ -0,0 +1,34 @@
+package com.traitor.ambatushop_10.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity @Table(name = "akun") @Getter @Setter
+public class akun {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Role role;
+
+    enum Role {
+        KASIR,
+        MANAJER,
+        ADMIN
+    }
+}
+
